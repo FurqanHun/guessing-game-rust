0882e0826abba3bb9adb7cbf8c5eace5e6abd748
0xQan <125211961+FurqanHun@users.noreply.github.com>
2024-09-15 19:14:44 +0500

    divided the codes in (fn) modules

--- src/main.rs
+++ src/main.rs
@@ -3,22 +3,11 @@ use std::cmp::Ordering;
 use std::io;
 fn main() {
     println!("Guess the number breh (1..100)!");
-    let secret_number = rand::thread_rng().gen_range(1..=100);
+    let secret_number = random_number();
 
     loop {
-        println!("Select a number: ");
-        let mut guess = String::new();
-        io::stdin()
-            .read_line(&mut guess)
-            .expect("Failed to read line");
-        let guess: u32 = match guess.trim().parse() {
-            Ok(num) => num,
-            Err(_) => {
-                println!("Please enter a number!");
-                continue;
-            }
-        };
-
+        println!("Select a number (1..100): ");
+        let guess = user_input();
         println!("You guessed: {}", guess);
 
         match guess.cmp(&secret_number) {
@@ -30,4 +19,23 @@ fn main() {
             }
         }
     }
+}
+
+fn random_number() -> u32 {
+    rand::thread_rng().gen_range(1..=100)
+}
+
+fn user_input() -> u32 {
+    let mut guess = String::new();
+    io::stdin()
+        .read_line(&mut guess)
+        .expect("Failed to read line");
+    let guess: u32 = match guess.trim().parse() {
+        Ok(num) => num,
+        Err(_) => {
+            println!("Please enter a number!");
+            user_input()
+        }
+    };
+    guess
 }
